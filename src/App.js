8fb23bb0040f6dc6ff582bcb41d0a8bb4c1fec4a
cheungky <kycheung1985@gmail.com>
2019-04-07 00:39:03 +0800

    add drawer menu

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import Drawer from '@material-ui/core/Drawer';
 
 const styles = {
   root: {
@@ -33,14 +33,30 @@ function Index() {
     return <h2>Users</h2>;
   }
 
-  function App() {
+  class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state={open:false}
+
+      this.handleDrawerIcon=this.handleDrawerIcon.bind(this);
+    }
+
+    handleDrawerIcon() {
+     this.setState({open: !this.state.open});
+
+    }
+
+
+
+  render(){
     return (
       <Router>
          <div className={styles.root}>
           {/*<nav>*/}
         <AppBar position="static">
           <Toolbar>
-            <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+            <IconButton className={styles.menuButton} color="inherit" aria-label="Menu" onClick={this.handleDrawerIcon}>
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" className={styles.grow}>
@@ -49,6 +65,23 @@ function Index() {
             <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
+        <Drawer
+          variant="persistent"
+          anchor="left"
+          open={this.state.open}
+        >
+        <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </Drawer>
         <switch>
           <Route path="/" exact component={Index} />
           <Route path="/about/" component={About} />
@@ -58,5 +91,6 @@ function Index() {
       </Router>
     );
   }
+}
   
   export default App;
